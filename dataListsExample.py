bf1a443419c545fd0e2c96a3875b5e0017886599
Gabriella Casanova <ggendec1@uncc.edu>
2018-02-19 18:37:20 -0500

    ListDataStructures

---
+++ dataListsExample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 17:47:16 2018
+
+@author: Gabriella
+"""
+list = [1,2,3,4]
+print (list)
+list.append(65)
+print(list[4])
+list.remove(3)
+print (list)
+list.insert(1, 200)
+print (list)
